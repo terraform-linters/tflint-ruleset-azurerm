a0659dd05cc207b5fbd2e998a8402fbde2eb0635
Amanda Kaufman <aka@microsoft.com>
2020-08-25 09:12:32 -0600

    Add custom rule for storage account names

---
+++ rules/azurerm_storage_account_invalid_name.go
@@ -0,0 +1,65 @@
+package rules
+
+import (
+	"fmt"
+	"regexp"
+
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+	"github.com/terraform-linters/tflint-ruleset-azurerm/project"
+)
+
+// AzurermStorageAccountInvalidNameRule checks the pattern is valid
+type AzurermStorageAccountInvalidNameRule struct {
+	resourceType  string
+	attributeName string
+	pattern       *regexp.Regexp
+}
+
+// NewAzurermStorageAccountInvalidNameRule returns new rule with default attributes
+func NewAzurermStorageAccountInvalidNameRule() *AzurermStorageAccountInvalidNameRule {
+	return &AzurermStorageAccountInvalidNameRule{
+		resourceType:  "azurerm_storage_account",
+		attributeName: "name",
+		pattern:       regexp.MustCompile(`^[a-z]{3, 24}$`),
+	}
+}
+
+// Name returns the rule name
+func (r *AzurermStorageAccountInvalidNameRule) Name() string {
+	return "azurerm_storage_account_invalid_name"
+}
+
+// Enabled returns whether the rule is enabled by default
+func (r *AzurermStorageAccountInvalidNameRule) Enabled() bool {
+	return true
+}
+
+// Severity returns the rule severity
+func (r *AzurermStorageAccountInvalidNameRule) Severity() string {
+	return tflint.ERROR
+}
+
+// Link returns the rule reference link
+func (r *AzurermStorageAccountInvalidNameRule) Link() string {
+	return project.ReferenceLink(r.Name())
+}
+
+// Check checks the pattern is valid
+func (r *AzurermStorageAccountInvalidNameRule) Check(runner tflint.Runner) error {
+	return runner.WalkResourceAttributes(r.resourceType, r.attributeName, func(attribute *hcl.Attribute) error {
+		var val string
+		err := runner.EvaluateExpr(attribute.Expr, &val)
+
+		return runner.EnsureNoError(err, func() error {
+			if !r.pattern.MatchString(val) {
+				runner.EmitIssueOnExpr(
+					r,
+					fmt.Sprintf(`"%s" does not match valid pattern %s`, truncateLongMessage(val), `^[a-z]{3, 24}$`),
+					attribute.Expr,
+				)
+			}
+			return nil
+		})
+	})
+}
--- rules/provider.go
+++ rules/provider.go
@@ -1,6 +1,8 @@
 package rules
 
 import (
+	"strings"
+
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 	"github.com/terraform-linters/tflint-ruleset-azurerm/rules/apispec"
 )
@@ -13,3 +15,17 @@ var Rules = append([]tflint.Rule{
 	NewAzurermWindowsVirtualMachineInvalidSizeRule(),
 	NewAzurermWindowsVirtualMachineScaleSetInvalidSkuRule(),
 }, apispec.Rules...)
+
+func truncateLongMessage(str string) string {
+	limit := 80
+
+	str = strings.Replace(str, "\r\n", "\n", -1)
+	str = strings.Replace(str, "\n", "\\n", -1)
+
+	r := []rune(str)
+	if len(r) > limit {
+		return string(r[0:limit]) + "..."
+	}
+
+	return str
+}
