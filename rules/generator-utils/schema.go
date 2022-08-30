44002f415aa77fa2cac4f58c06e03f6b372d8d0b
Guy Bertental <gubert@microsoft.com>
2022-08-30 23:52:20 +0300

    linter issues

--- rules/generator-utils/schema.go
+++ rules/generator-utils/schema.go
@@ -5,33 +5,33 @@ import (
 	"io/ioutil"
 )
 
-// "provider_schemas" describes the provider schemas for all providers throughout the configuration tree.
+// Schema describes the provider schemas for all providers throughout the configuration tree.
 type Schema struct {
 	ProviderSchemas ProviderSchemas `json:"provider_schemas"`
 }
 
-// "provider_schemas" describes the provider schemas for all providers throughout the configuration tree.
+// ProviderSchemas describes the provider schemas for all providers throughout the configuration tree.
 type ProviderSchemas struct {
 	Azurerm ProviderSchema `json:"registry.terraform.io/hashicorp/azurerm"`
 }
 
-// "resource_schemas" describes the schemas for all resources.
+// ProviderSchema describes the schemas for all resources.
 type ProviderSchema struct {
 	ResourceSchemas map[string]ResourceSchema `json:"resource_schemas"`
 }
 
-// "resource_schemas" describes the schemas for all resources.
+// ResourceSchema describes the schemas for all resources.
 type ResourceSchema struct {
 	Block BlockSchema `json:"block"`
 }
 
-// A block representation contains "attributes" and "block_types" (which represent nested blocks).
+// BlockSchema A block representation contains "attributes" and "block_types" (which represent nested blocks).
 type BlockSchema struct {
 	Attributes map[string]AttributeSchema `json:"attributes"`
 	BlockTypes map[string]ResourceSchema  `json:"block_types"`
 }
 
-// AttributeSchema
+// AttributeSchema contains Type and Sensitive
 type AttributeSchema struct {
 	Type      interface{} `json:"type"`
 	Sensitive bool        `json:"sensitive"`
